b56d0ac01432d1f2f2b3f1a6added3000c2b9641
Siser-Pratap <siserinsevoc@gmail.com>
2026-04-26 01:14:42 +0530

    Fix next config to use experimental.serverComponentsExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ['yt-search', 'cheerio'],
+  experimental: {
+    serverComponentsExternalPackages: ['yt-search', 'cheerio'],
+  },
 };
 
 export default nextConfig;
